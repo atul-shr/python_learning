9d8e5142af3a3cafee66225cf4351cfc3aecb939
atul-shr <40176783+atul-shr@users.noreply.github.com>
2019-09-10 16:53:24 +0530

    Update json_read.py

--- json_read.py
+++ json_read.py
@@ -1,5 +1,30 @@
 import json
 
+response = """{
+            "agreement" : [
+            { "usageCD" : null,
+              "relationshipID" : null,
+              "account" : {
+                "bookOfBusinessCD" : "RM03",
+                "accountTypeCd" : "TFSA"              
+              }
+            
+            }
+            ],
+            "streetAddressess" :[
+              {
+                "uniqueId":1,
+                "address": {
+                        "streetNum":"535",
+                        "streetName": "THE EAST MALL"                  }
+                "lastUpdate" : {
+                    "userId" : "wcom_user"              }
+              }
+            ]
+            }"""
+
+
+
 res = json.loads(response)
 
 for doc in res:
