99a52384dcd6c0a0a3fa091f6178ee7a15937b43
Atul Sharma <atulsharma131@gmail.com>
2019-12-13 00:52:04 +0530

    changes with chunks

--- VSS/bin/readChunks.py
+++ VSS/bin/readChunks.py
@@ -11,10 +11,6 @@ df_dur = pd.DataFrame()
 for chunks in (pd.read_sql(sql='select * from inventory',con=con,chunksize=10000)):
     current_time = datetime.datetime.now()
     dur = current_time - old_time
-    df_dur['DURATION'] = dur.total_seconds()
-     
-    # print('Duration time is : ',dur.total_seconds()) 
+    print('Duration time is : ',dur.total_seconds()) 
     chunks.to_csv('bigfile.csv',mode='a',index=False)
     old_time =  current_time
-
-print(df_dur)
