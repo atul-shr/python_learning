8acdc5e52312fbc475ca75d48815fa794d8025dd
atul-shr <40176783+atul-shr@users.noreply.github.com>
2020-04-09 23:25:37 +0530

    added

--- addCols.py
+++ addCols.py
@@ -5,13 +5,14 @@ directory = 'D:\\learning\\code\\utils\\addColumnExcel'
 
 for filename in os.listdir(directory):
     if filename.endswith(".csv"):
-        with open(directory + '\\' + filename) as input_file:
-            # header = input_file.readline()
-            data = pd.read_csv(input_file, sep=",")
-        data.insert(4, "New Data", 10)
+        try:
+            with open(directory + '\\' + filename) as input_file:
+                # header = input_file.readline()
+                data = pd.read_csv(input_file, sep=",")
+            data.insert(4, "New Data", 10)
 
-        with open(directory + '\\' + filename, "w") as output_file:
-            # output_file.write(header)
-            data.to_csv(output_file, index=False,line_terminator="\n")        
-
-            
+            with open(directory + '\\' + filename, "w") as output_file:
+                # output_file.write(header)
+                data.to_csv(output_file, index=False,line_terminator="\n")
+        except:
+            pass
