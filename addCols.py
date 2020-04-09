d3cba1c211245b82303341b23eb051a304d5b8d6
atul-shr <40176783+atul-shr@users.noreply.github.com>
2020-04-09 23:04:29 +0530

    add col by python

---
+++ addCols.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+
+directory = 'D:\\learning\\code\\utils\\addColumnExcel'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        with open(directory + '\\' + filename) as input_file:
+            # header = input_file.readline()
+            data = pd.read_csv(input_file, sep=",")
+        data.insert(4, "New Data", 10)
+
+        with open(directory + '\\' + filename, "w") as output_file:
+            # output_file.write(header)
+            data.to_csv(output_file, index=False,line_terminator="\n")        
+
+            
