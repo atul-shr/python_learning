ba7e2bb43824f00fcdfc444047c948d1607f5105
atul-shr <40176783+atul-shr@users.noreply.github.com>
2020-04-10 20:36:16 +0530

    changes

--- moveFiles.py
+++ moveFiles.py
@@ -2,6 +2,15 @@ import os
 import sys
 import fnmatch
 import shutil
+from datetime import datetime
+from datetime import timedelta
+from dateutil.relativedelta import relativedelta
+
+def get_month_day_range(date):
+    last_day = date + relativedelta(day=1, months=+1, days=-1)
+    first_day = date + relativedelta(day=1)
+    return first_day, last_day
+
 
 # Directory Paths 
 
@@ -19,59 +28,67 @@ pt_cross_tgt_dir = "C:\\Users\\atul.shr\\Documents\\Email Attachments\\PT"
 
 print("Loop through files in " + directory + "\n")
 
+today = datetime.today()
+first = today.replace(day=1)
+lastMonth = first - timedelta(days=1)
+
+firstLastMonth, LastLastMonth = get_month_day_range(lastMonth)
+
 for filename in os.listdir(directory):
     if os.path.isdir(directory + '\\' + filename):
-        print(filename + " is a directory "  )
+        print(filename + " is a directory " )
     else:
-        if fnmatch.fnmatch(filename, '*-DI-*Material*'):
-            try:
-                os.rename(directory + '\\' + filename, di_mkt_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + di_mkt_tgt_dir)
-            except:
-                print(filename + " already present at " + di_mkt_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-DI-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, di_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + di_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + di_cross_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-FP-*Material*'):
-            try:
-                os.rename(directory + '\\' + filename, fp_mkt_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + fp_mkt_tgt_dir)
-            except:
-                print(filename + " already present at " + fp_mkt_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-FP-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, fp_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + fp_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + fp_cross_tgt_dir)            
-        elif fnmatch.fnmatch(filename, '*-IT-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, it_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + it_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + it_cross_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-PIA-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, pia_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + pia_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + pia_cross_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-PIC-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, pic_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + pic_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + pic_cross_tgt_dir)
-        elif fnmatch.fnmatch(filename, '*-PT-*Cross*'):
-            try:
-                os.rename(directory + '\\' + filename, pt_cross_tgt_dir + '\\' + filename)
-                print(filename + " moved to " + pt_cross_tgt_dir)
-            except:
-                print(filename + " already present at " + pt_cross_tgt_dir)
-            
+        if datetime.strptime(filename[:10],'%Y-%m-%d')>=  firstLastMonth.replace(hour=0, minute=0, second=0, microsecond=0) and datetime.strptime(filename[:10],'%Y-%m-%d') <= LastLastMonth.replace(hour=0, minute=0, second=0, microsecond=0):
+            if fnmatch.fnmatch(filename, '*-DI-*Material*'):
+                try:
+                    os.rename(directory + '\\' + filename, di_mkt_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + di_mkt_tgt_dir)
+                except:
+                    print(filename + " already present at " + di_mkt_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-DI-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, di_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + di_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + di_cross_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-FP-*Material*'):
+                try:
+                    os.rename(directory + '\\' + filename, fp_mkt_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + fp_mkt_tgt_dir)
+                except:
+                    print(filename + " already present at " + fp_mkt_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-FP-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, fp_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + fp_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + fp_cross_tgt_dir)            
+            elif fnmatch.fnmatch(filename, '*-IT-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, it_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + it_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + it_cross_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-PIA-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, pia_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + pia_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + pia_cross_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-PIC-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, pic_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + pic_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + pic_cross_tgt_dir)
+            elif fnmatch.fnmatch(filename, '*-PT-*Cross*'):
+                try:
+                    os.rename(directory + '\\' + filename, pt_cross_tgt_dir + '\\' + filename)
+                    print(filename + " moved to " + pt_cross_tgt_dir)
+                except:
+                    print(filename + " already present at " + pt_cross_tgt_dir)
+                
+            else:
+                print(filename + " does not satisfy any pattern hence not moved. " + str(datetime.strptime(filename[:10],'%Y-%m-%d')))
         else:
-            print(filename + " does not satisfy any pattern hence not moved. ")
-
+            print(filename + " is older than last month. ")
