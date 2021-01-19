a6ddd2350ed7d271c5c07a39c29783e6a64a6e8d
Atul Sharma <atulsharma131@gmail.com>
2021-01-19 12:15:36 +0530

    commit transactions bi-temporal

--- code/create_portfolio.py
+++ code/create_portfolio.py
@@ -24,23 +24,43 @@ scope_port = "cut1-filpoc-9999"
 
 print(datetime.now())
 
-for index,row in port_df.iterrows():
-    # print(row['display_name'],row['code'],row['base_currency'])
-    # print(all_port[0],all_port[1],all_port[2],all_port[3])
+# for index,row in port_df.iterrows():
+#     # print(row['display_name'],row['code'],row['base_currency'])
+#     # print(all_port[0],all_port[1],all_port[2],all_port[3])
 
-    # details of the new portfolio to be created, created here with the minimum set of mandatory fields
-    request = models.CreateTransactionPortfolioRequest(
-        # descriptive name for the portfolio
-        display_name=row['display_name'],
-        # unique portfolio code, portfolio codes must be unique across scopes
-        code=row['code'],
-        base_currency=row['base_currency'])
+#     # details of the new portfolio to be created, created here with the minimum set of mandatory fields
+#     request = models.CreateTransactionPortfolioRequest(
+#         # descriptive name for the portfolio
+#         display_name=row['display_name'],
+#         # unique portfolio code, portfolio codes must be unique across scopes
+#         code=row['code'],
+#         base_currency=row['base_currency'])
 
-    # create the portfolio in LUSID in the specified scope
-    result = transaction_portfolios_api.create_portfolio(
-        scope=scope_port,
-        create_transaction_portfolio_request=request)
+#     # create the portfolio in LUSID in the specified scope
+#     result = transaction_portfolios_api.create_portfolio(
+#         scope=scope_port,
+#         create_transaction_portfolio_request=request)
 
 print(datetime.now())
 
+
+# create the portfolio
+# scope = "finbourne"
+guid = str(uuid.uuid4())
+effective_date = datetime(2018, 1, 1, tzinfo=pytz.utc)
+
+request = models.CreateTransactionPortfolioRequest(
+    display_name="portfolio-filpoc-X001",
+    code="source1-filpoc-X001",
+    base_currency="GBP",
+    created=effective_date
+)
+
+result = transaction_portfolios_api.create_portfolio(
+    scope=scope_port, 
+    create_transaction_portfolio_request=request)
+
+portfolio_id = result.id.code
+
+print(portfolio_id)
 
